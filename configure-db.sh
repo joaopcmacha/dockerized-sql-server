d8619b3999ea2740338b40059024c7a2bf0b384d
joao.machaqueiro <joaomachaqueiro@jce.com.pt>
2025-03-18 15:26:11 +0000

    update to SQL Server 2022
    updated to SQL Server 2022 because 2017 is incompatible with ubuntu 24.04 host

--- configure-db.sh
+++ configure-db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Wait 60 seconds for SQL Server to start up by ensuring that 
+# Wait 180 seconds for SQL Server to start up by ensuring that 
 # calling SQLCMD does not return an error code, which will ensure that sqlcmd is accessible
 # and that system and user databases return "0" which means all databases are in an "online" state
 # https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-databases-transact-sql?view=sql-server-2017 
@@ -9,15 +9,15 @@ DBSTATUS=1
 ERRCODE=1
 i=0
 
-while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt 60 ]] && [[ $ERRCODE -ne 0 ]]; do
+while (( DBSTATUS != 0 && i < 180 && ERRCODE != 0 )); do
 	i=$i+1
-	DBSTATUS=$(/opt/mssql-tools/bin/sqlcmd -h -1 -t 1 -U sa -P $SA_PASSWORD -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases")
+	DBSTATUS=$(/opt/mssql-tools18/bin/sqlcmd -C -h -1 -t 1 -U sa -P $SA_PASSWORD -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases")
 	ERRCODE=$?
 	sleep 1
 done
 
-if [ $DBSTATUS -ne 0 ] || [ $ERRCODE -ne 0 ]; then 
-	echo "SQL Server took more than 60 seconds to start up or one or more databases are not in an ONLINE state"
+if (( $DBSTATUS != 0 )) || (( $ERRCODE != 0 )); then 
+	echo "SQL Server took more than 180 seconds to start up or one or more databases are not in an ONLINE state"
 	exit 1
 fi
 
@@ -32,4 +32,4 @@ for relative_file_path in /db_files/*.mdf; do
 done
 
 # # Run the setup script to create the DB and the schema in the DB
- /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i setup.sql
+ /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -P $SA_PASSWORD -d master -i setup.sql
